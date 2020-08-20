c0e619860efeaa37191d465a5405fe89d9bba312
MrKneetjie <david.knetemann@gmail.com>
2020-08-20 20:52:16 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }))
+        .catch(err => err);
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p className="App-intro">{this.state.apiResponse}</p>
+          <form>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username"></input>
+
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password"></input>
+
+            <div class="buttons">
+              <button type="submit">Log In</button>
+              <button>Sign Up</button>
+            </div>
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
